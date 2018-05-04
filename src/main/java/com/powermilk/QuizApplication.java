b2a8f4cfd0499037eaa7ed39036dd190caea6c46
Rafał Kociniewski <rafal.kociniewski@gmail.com>
2018-05-04 23:28:01 +0200

    little changes in main application

--- src/main/java/com/powermilk/QuizApplication.java
+++ src/main/java/com/powermilk/QuizApplication.java
@@ -1,18 +1,11 @@
 package com.powermilk;
 
-import com.powermilk.service.CorrectQuestionValidatorService;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
-@ComponentScan(basePackageClasses = {
-        CorrectQuestionValidatorService.class,
-})
+@ComponentScan
 @SpringBootApplication
-@EnableAutoConfiguration
-@PropertySource("application.properties")
 public class QuizApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuizApplication.class, args);
