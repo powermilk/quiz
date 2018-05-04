dd45e57fe1badf3c704694969f1a75cce64cbd9e
Rafał Kociniewski <rafal.kociniewski@gmail.com>
2018-05-04 19:39:19 +0200

    reorganize imports

--- src/test/java/com/powermilk/repository/QuestionRepositoryTest.java
+++ src/test/java/com/powermilk/repository/QuestionRepositoryTest.java
@@ -2,15 +2,12 @@ package com.powermilk.repository;
 
 import com.powermilk.TestEntities;
 import com.powermilk.model.Question;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
